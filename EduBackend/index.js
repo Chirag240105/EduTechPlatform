4c839e61c131ca4b9636217da4e74a8824a48542
Chirag240105 <pandeychirag163@gmail.com>
2026-02-19 10:41:00 +0530

    Update CORS configuration to allow all origins

--- EduBackend/index.js
+++ EduBackend/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
-    origin: process.env.URL,
+    origin: true,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
